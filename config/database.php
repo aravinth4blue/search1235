0e028fcc1397616d5e523b78b7763702b98a59df
Aravinth <aravinth.krishnamoorthi@gmail.com>
2019-03-17 11:24:08 +0530

    Database settings added

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'searchapp1237'),
+            'username' => env('DB_USERNAME', 'searchapp1237'),
+            'password' => env('DB_PASSWORD', 'searchapp1237'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
